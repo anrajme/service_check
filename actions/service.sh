f90c255417f758d36b6f2cefd0ea0bfa56198dfe
anraj <anraj@adobe.com>
2022-01-19 01:23:41 +1100

    update actions service

--- actions/service.sh
+++ actions/service.sh
@@ -1,33 +1,24 @@
-#service $2 $1
 function get_linux_distribution() {
   distro=`lsb_release -s -i`
 }
 
-
-function main() {
 act=$1
 service=$2
-echo $act
-echo $service
+
 if [ $# -lt 2 ]
   then
     echo "Usage: service.py <action> <service>"
     exit 0
 fi
+
 get_linux_distribution
-osystem=$distro
 
-
-if [ "$osystem" ] == "Ubuntu"
+if [ $distro == 'Ubuntu' ]
   then
     /etc/init.d/$service $act
-elif [ $osystem == "Redhat" ] || [ $osystem == "Fedora" ] || [ $osystem == "CentOS" ]
+elif [[ $distro == "Redhat" ||  $distro == "Fedora"  ||  $distro == "CentOS" ]]
   then
-  systemctl $service $act
+    systemctl $service $act
 else
    echo "Unsupported Operating System"
 fi
-
-}
-
-main
